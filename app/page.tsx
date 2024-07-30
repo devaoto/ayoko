ccb93c1720b9bc4cc69f08621e3a8290541efbc4
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-07-30 09:19:56 +0600

    Ad all-time popular and trending

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
 import { use } from "react";
 
 import { Hero } from "@/components/home/spotlight";
-import { getSpotlight } from "@/lib/anime";
+import { getPopular, getSpotlight, getTrending } from "@/lib/anime";
+import { Cards } from "@/components/home/cards";
 
 export default function Home() {
-  const spotlight = use(getSpotlight());
+  const [spotlight, trending, popular] = use(
+    Promise.all([getSpotlight(), getTrending(), getPopular()]),
+  );
 
   return (
     <div>
       <Hero anime={spotlight} />
+      <div className="mt-10">
+        <h1 className="mb-2 select-none text-3xl font-bold">Trending Anime</h1>
+        <Cards animes={trending} />
+      </div>
+      <div className="mt-10">
+        <h1 className="mb-2 select-none text-3xl font-bold">
+          All Time Popular
+        </h1>
+        <Cards animes={popular} />
+      </div>
     </div>
   );
 }
--- lib/anime.ts
+++ lib/anime.ts
@@ -1,13 +1,16 @@
 import ky from "ky";
+import NodeCache from "node-cache";
 
 import { Spotlight } from "@/types/spotlight";
 import { ANIFY_URL } from "@/config/api";
+import { AnimeCard } from "@/types/cards";
 
 export const dynamicParams = false;
 
+const cache = new NodeCache({ stdTTL: 5 * 1800 });
+
 const noCacheFetch = (input: RequestInfo | URL, init?: RequestInit) => {
-  const customInit = { ...init, cache: "no-store" };
-  // @ts-ignore
+  const customInit = { ...init, cache: "no-store" } as RequestInit;
 
   return fetch(input, customInit);
 };
@@ -19,12 +22,88 @@ const anify = ky.create({
 });
 
 export async function getSpotlight(): Promise<Spotlight[]> {
+  const cacheKey = "spotlight";
+
+  if (cache.get(cacheKey) && typeof cache.get(cacheKey) === "string") {
+    return JSON.parse(cache.get(cacheKey)!) as Spotlight[];
+  }
+
+  if (
+    cache.get(cacheKey) &&
+    typeof cache.get(cacheKey) === "string" &&
+    JSON.parse(cache.get(cacheKey)!).length <= 0
+  ) {
+    cache.del(cacheKey);
+
+    return await getSpotlight();
+  }
+
   const res = await anify.get(
     "seasonal/anime?fields=[id,title,bannerImage,description,artwork,trailer]",
   );
-  const data = await res.json<{ trending: Spotlight[] }>();
 
+  const data = await res.json<{ trending: Spotlight[] }>();
   const spotlight: Spotlight[] = data.trending;
+
+  cache.set(cacheKey, JSON.stringify(spotlight));
 
   return spotlight;
 }
+
+export async function getTrending(): Promise<AnimeCard[]> {
+  const cacheKey = "trending";
+
+  if (cache.get(cacheKey) && typeof cache.get(cacheKey) === "string") {
+    return JSON.parse(cache.get(cacheKey)!) as AnimeCard[];
+  }
+
+  if (
+    cache.get(cacheKey) &&
+    typeof cache.get(cacheKey) === "string" &&
+    JSON.parse(cache.get(cacheKey)!).length <= 0
+  ) {
+    cache.del(cacheKey);
+
+    return await getTrending();
+  }
+
+  const res = await anify.get(
+    "seasonal/anime?fields=[id,title,bannerImage,description,artwork,trailer,coverImage,season]",
+  );
+
+  const data = await res.json<{ trending: AnimeCard[] }>();
+  const trending: AnimeCard[] = data.trending;
+
+  cache.set(cacheKey, JSON.stringify(trending));
+
+  return trending;
+}
+
+export const getPopular = async (): Promise<AnimeCard[]> => {
+  const cacheKey = "popular";
+
+  if (cache.get(cacheKey) && typeof cache.get(cacheKey) === "string") {
+    return JSON.parse(cache.get(cacheKey)!) as AnimeCard[];
+  }
+
+  if (
+    cache.get(cacheKey) &&
+    typeof cache.get(cacheKey) === "string" &&
+    JSON.parse(cache.get(cacheKey)!).length <= 0
+  ) {
+    cache.del(cacheKey);
+
+    return await getPopular();
+  }
+
+  const res = await anify.get(
+    "seasonal/anime?fields=[id,title,bannerImage,description,artwork,trailer,coverImage,season]",
+  );
+
+  const data = await res.json<{ popular: AnimeCard[] }>();
+  const popular: AnimeCard[] = data.popular;
+
+  cache.set(cacheKey, JSON.stringify(popular));
+
+  return popular;
+};
