4f4b3835df40154af979ad94f5c42ace63319f0f
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-07-30 09:18:33 +0600

    Create card types

---
+++ types/cards.ts
@@ -0,0 +1,22 @@
+export interface CardArtwork {
+  img: string;
+  type: string;
+  providerId: "anilist" | "tvdb" | "tmdb" | "kitsu";
+}
+
+export interface CardTitle {
+  romaji: string;
+  native: string;
+  english: string;
+}
+
+export interface AnimeCard {
+  id: string;
+  title: CardTitle;
+  coverImage: string;
+  bannerImage: string;
+  description: string;
+  trailer: string;
+  season: string;
+  artwork: CardArtwork[];
+}
