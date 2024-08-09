81d01d8b1c45f3b8268e61cf342aaf96c2e5468e
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-08-09 22:13:05 +0600

    Add a bit more information on watch page

--- app/watch/[id]/page.tsx
+++ app/watch/[id]/page.tsx
@@ -1,7 +1,9 @@
+import { use } from "react";
+
 import { Player } from "@/components/player/player";
 import { WatchPageEpisodes } from "@/components/watch/watchPageEpisodes";
 import { getEpisodes, getInfo, getSources } from "@/lib/anime";
-import { use } from "react";
+import { changeStatus, changeSeason } from "@/lib/utils";
 
 export default function Page({
   params,
@@ -40,29 +42,62 @@ export default function Page({
     ].find((p) => Number(p.number) === Number(searchParams.number));
 
   return (
-    <div className="flex justify-between gap-10">
-      <div className="max-w-4xl">
-        <Player
-          sources={sources.sources}
-          subtitles={sources.subtitles}
-          title={currentEpisode?.title!}
-          poster={currentEpisode?.image!}
-        />
-        <h1 className="text-2xl font-medium">
-          {info.title.english ?? info.title.romaji}: Episode{" "}
-          {currentEpisode?.number}
-        </h1>
-        <h2 className="text-foreground-500">{currentEpisode?.title}</h2>
+    <>
+      <div className="mt-4 flex flex-col justify-between gap-10 md:flex-row">
+        <div className="md:min-w-4xl w-full md:max-w-4xl">
+          <Player
+            episodeId={searchParams.episodeId}
+            episodeNumber={Number(searchParams.number)}
+            id={params.id}
+            poster={currentEpisode?.image!}
+            provider={searchParams.server}
+            sources={sources.sources}
+            subType={searchParams.subType}
+            subtitles={sources.subtitles}
+            title={currentEpisode?.title!}
+          />
+          <h1 className="text-2xl font-medium">
+            Episode {currentEpisode?.number}: {currentEpisode?.title}
+          </h1>
+          <h2 className="text-foreground-500">
+            {info.title.english ?? info.title.romaji}
+          </h2>
+        </div>
+        <div className="h-[530px] !max-h-[510px] overflow-y-scroll scrollbar-hide">
+          <WatchPageEpisodes
+            currentEpisode={searchParams.number}
+            episodes={currentProvider!}
+            id={params.id}
+            provider={searchParams.server}
+            subType={searchParams.subType}
+          />
+        </div>
       </div>
-      <div className="h-[530px] !max-h-[510px] overflow-y-scroll scrollbar-hide">
-        <WatchPageEpisodes
-          episodes={currentProvider!}
-          subType={searchParams.subType}
-          id={params.id}
-          provider={searchParams.server}
-          currentEpisode={searchParams.number}
+      <div className="mt-10">
+        <h1 className="text-2xl font-bold">Description</h1>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: currentEpisode?.description ?? "",
+          }}
+          className="w-full md:max-w-4xl"
         />
+        <div className="mt-5">
+          <h1 className="text-2xl font-bold">Overview</h1>
+          <ul className="list-inside list-disc">
+            <li>Status: {changeStatus(info.status)}</li>
+            <li>Season: {changeSeason(info.season)}</li>
+            <li>Year: {info.year}</li>
+            <li>Country of Origin: {info.countryOfOrigin}</li>
+            <li>Format: {info.format}</li>
+            <li>Duration: {info.duration} minutes</li>
+            <li>Current Episode: {info.currentEpisode}</li>
+            <li>Total Episodes: {info.totalEpisodes}</li>
+            <li>Popularity (TMDb): {info.popularity.tmdb}</li>
+            <li>Popularity (MAL): {info.popularity.mal}</li>
+            <li>Popularity (AniList): {info.popularity.anilist}</li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
