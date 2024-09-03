b52116dbd8a3c3f3ef450e03f0421adf9250d874
Mohtasim Alam Sohom <94981761+devaoto@users.noreply.github.com>
2024-09-03 22:03:13 +0600

    Update progress.tsx

--- components/progress.tsx
+++ components/progress.tsx
@@ -1,17 +1,14 @@
 "use client";
 
-import { Suspense } from "react";
 import { AppProgressBar } from "next-nprogress-bar";
 
 export default function ProgressBar() {
   return (
-    <Suspense>
-      <AppProgressBar
-        shallowRouting
-        color="#4A90E2"
-        height="4px"
-        options={{ showSpinner: false }}
-      />
-    </Suspense>
+    <AppProgressBar
+      shallowRouting
+      color="#4A90E2"
+      height="4px"
+      options={{ showSpinner: false }}
+    />
   );
 }
