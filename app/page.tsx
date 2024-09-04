9df4798bc1e0188a428a9236800d9d654dcd526e
Mohtasim Alam Sohom <94981761+devaoto@users.noreply.github.com>
2024-09-04 22:37:14 +0600

    Improve home page layout with NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -3,29 +3,31 @@ import { use } from "react";
 import { Hero } from "@/components/home/spotlight";
 import { getSeasonal } from "@/lib/anime";
 import { Cards } from "@/components/home/cards";
-import SideBar from "@/components/sidebar";
+import { Navbar } from "@/components/navbar";
 
 export default function Home() {
   const seasonal = use(getSeasonal());
 
   return (
-    <div className="relative mb-32 flex h-screen min-h-screen flex-col sm:mb-0">
-      <SideBar />
-      <div className="container mx-auto max-w-7xl flex-grow px-2">
+    <>
+      <Navbar navFor="home" />
+      <div className="relative mb-32 flex h-screen min-h-screen flex-col sm:mb-0">
         <Hero anime={seasonal.trending} />
-        <div className="mt-10">
-          <h1 className="mb-2 select-none text-3xl font-bold">
-            Trending Anime
-          </h1>
-          <Cards animes={seasonal.trending} />
-        </div>
-        <div className="mt-10">
-          <h1 className="mb-2 select-none text-3xl font-bold">
-            All Time Popular
-          </h1>
-          <Cards animes={seasonal.popular} />
+        <div className="container flex-grow px-4 py-8">
+          <div className="mt-10">
+            <h1 className="mb-2 select-none text-3xl font-bold">
+              Trending Anime
+            </h1>
+            <Cards animes={seasonal.trending} />
+          </div>
+          <div className="mt-10">
+            <h1 className="mb-2 select-none text-3xl font-bold">
+              All Time Popular
+            </h1>
+            <Cards animes={seasonal.popular} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
