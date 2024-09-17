ac29ef10320b13978d1a0ca6449ac2a46e16f0f0
Mohtasim Alam Sohom <94981761+devaoto@users.noreply.github.com>
2024-09-17 23:45:45 +0600

    Update error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -10,7 +10,6 @@ export default function Err({
   reset: () => void;
 }>) {
   useEffect(() => {
-    // Log the error to an error reporting service
     /* eslint-disable no-console */
     console.error(error);
   }, [error]);
